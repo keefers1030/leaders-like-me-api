35424ecc1e7e9df8d00d2e5a152e3ef5f1f4df63
Keith March Mistler <kmistler@gmail.com>
2021-05-10 13:20:58 -0400

    changed index.js and changed pug index

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+//app.get('/leaders/:slug', getLeader)
+
+//app.post('/leaders', express.json(), addNewLeader)
+
+app.all('*', (req, res) => {
+  return res.sendStatus(404)
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 });
+
+
+
+
+
+
