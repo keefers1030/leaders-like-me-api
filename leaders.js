1b8fe7f7199d4fe1926c67f0378a4e4204d74505
Keith March Mistler <kmistler@gmail.com>
2021-05-13 21:04:51 -0400

    adding to DB

--- leaders.js
+++ leaders.js
@@ -4,74 +4,111 @@ const leaders = [{
   category: 'MATH',
   community: 'BIPOC',
   link: 'https://www.nasa.gov/content/katherine-johnson-biography',
-  slug: 'katherine-johnson',
+  slug: 'katherine-johnson'
 }, {
   name: 'Alan Turing',
   image: 'images/aturing.jpg',
   category: 'ENGINEERING',
   community: 'LGBTQIA',
-  link: 'https://en.wikipedia.org/wiki/Alan_Turing'
+  link: 'https://en.wikipedia.org/wiki/Alan_Turing',
+  slug: 'alan-turing'
+
 },{
   name: 'Ada Lovelace',
   image: 'images/alovelace.jpg',
   category: 'ENGINEERING',
   community: 'FEMALE',
-  link: 'https://www.computerhistory.org/babbage/adalovelace/'
+  link: 'https://www.computerhistory.org/babbage/adalovelace/',
+  slug: 'ada-lovelace'
+
 }
 ,{
   name: 'Stephen Hawking',
   image: 'images/shawking.jpg',
   category: 'SCIENCE',
   community: 'DISABLED',
-  link: 'https://www.frontiersofknowledgeawards-fbbva.es/galardonados/stephen-hawking-2/'
+  link: 'https://www.frontiersofknowledgeawards-fbbva.es/galardonados/stephen-hawking-2/',
+  slug: 'stephen-hawking'
 },
 {
   name: 'Florence Price',
   image: 'images/fprice.jpg',
   category: 'ARTS',
   community: 'BIPOC',
-  link: 'https://www.classicfm.com/discover-music/florence-price/'
+  link: 'https://www.classicfm.com/discover-music/florence-price/',
+  slug: 'florence-price'
 },
 {
   name: 'Dolores Huerta',
   image: 'images/dhuerta.jpg',
   category: 'SOCIAL JUSTICE',
   community: 'BIPOC', // female
-  link: 'https://doloreshuerta.org/dolores-huerta/'
+  link: 'https://doloreshuerta.org/dolores-huerta/',
+  slug: 'dolors-huerta'
 },
 {
   name: 'Nicole Cardoza',
-  image: 'images/ncardozajpg',
+  image: 'images/ncardozajpg.jpg',
   category: 'SOCIAL JUSTICE',
   community: 'BIPOC', // female, mental health
-  link: 'https://www.nicoleacardoza.com/'
+  link: 'https://www.nicoleacardoza.com/',
+  slug: 'nicole-cardoza'
 },
 {
   name: 'Brittany Packnett Cunningham',
-  image: 'images/bpacknettcunningham',
+  image: 'images/bpacknettcunningham.jpg',
   category: 'SOCIAL JUSTICE',
   community: 'BIPOC', // female
-  link: 'https://brittanypacknett.com/'
+  link: 'https://brittanypacknett.com/',
+  slug: 'brittany-cunningham'
 },
 {
   name: 'Rachel Cargle',
-  image: 'images/rcargle',
+  image: 'images/rcargle.jpg',
   category: 'SOCIAL JUSTICE', //writer
   community: 'BIPOC', // female
-  link: 'https://rachel-cargle.com/'
+  link: 'https://rachel-cargle.com/',
+  slug: 'rachel-cargle'
 },
 {
   name: 'Ceyenne Doroshow',
-  image: 'images/cdoroshow',
+  image: 'images/cdoroshow.jpg',
   category: 'SOCIAL JUSTICE',
   community: 'BIPOC', // LGBTQIA
-  link: 'https://www.gq.com/story/ceyenne-doroshow-glits-founder-profile'
+  link: 'https://www.gq.com/story/ceyenne-doroshow-glits-founder-profile',
+  slug: 'ceyenne-doroshow'
 },
 {
   name: 'Cathy Park Hong',
-  image: 'images/cdoroshow',
+  image: 'images/cdoroshow.jpg',
   category: 'SOCIAL JUSTICE', // PERFORMING ART
   community: 'AAPI', // FEMALE
-  link: 'http://www.cathyparkhong.com/'
+  link: 'http://www.cathyparkhong.com/',
+  slug: 'cathy-park-hong'
 },
+{
+  name: 'Jonny Sun',
+  image: 'images/jsun.jpg',
+  category: 'ARTS', // Writer/activist
+  community: 'AAPI', // LGBTQIA
+  link: 'https://jonnysun.com/',
+  slug: 'jonny-sun'
+},
+{
+  name: 'Amanda Gorman',
+  image: 'images/agorman.jpg',
+  category: 'ARTS', //activist
+  community: 'BIPOC', // Female
+  link: 'https://www.theamandagorman.com/',
+  slug: 'amanda-gorman'
+},
+{
+  name: 'Mimi Zhu',
+  image: 'images/mzhu.jpg',
+  category: 'ARTS', //activist
+  community: 'AAPI', // LGBTQIA
+  link: 'https://www.instagram.com/mimizhuxiyuan/',
+  slug: 'mimi-zhu'
+},
+
 ]
